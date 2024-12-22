0d6c911fe36047260627c1a373167a2da423ff25
Rebeccacheptoek <rebbytoek095@gmail.com>
2024-12-23 00:07:16 +0300

    compose.yaml changes

--- tests/Feature/ProductTest.php
+++ tests/Feature/ProductTest.php
@@ -24,7 +24,7 @@ class ProductTest extends TestCase
                     '*' => [
                         'id',
                         'name',
-                        'description',
+                        'details',
                         'price',
                         'stock',
                         'image_url',
